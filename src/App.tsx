f8231696489b2cecdecd5c39d9d48f931472037e
yuuki-katsuta <reptpr0314@gmail.com>
2021-05-04 01:06:45 +0900

    返却値・変数の型指定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./styles.css";
 import { Practice1 } from "./practices/Practice1";
+import { Practice2 } from "./practices/Practice2";
+import { Practice3 } from "./practices/Practice3";
 
 export default function App() {
   return (
     <div className="App">
       <Practice1 />
+      <Practice2 />
+      <Practice3 />
     </div>
   );
 }
---
+++ src/practices/Practice3.tsx
@@ -0,0 +1,17 @@
+export const Practice3 = () => {
+  const getTotalFee = (num: number) => {
+    const total = num * 1.1;
+    return total;
+  };
+  const onClickPractice = () => {
+    let total: number = 0; //初期化
+    total = getTotalFee(1000);
+    console.log(total);
+  };
+  return (
+    <div>
+      <p>練習問題2・変数の型指定</p>
+      <button onClick={onClickPractice}>実行</button>
+    </div>
+  );
+};
